7c727c6ed2cc74f05e6865cefb182fd0e935b0b3
Markus Mayer <widemeadows@gmail.com>
2020-10-17 20:49:53 +0200

    Fix warnings in main.rs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,11 @@
 use graph_based_image_segmentation::segmentation::{EuclideanRGB, MagicThreshold, Segmentation};
-use opencv::core::{Point, Scalar, Vector, no_array, min_max_loc, scale_add, CV_64F, CV_8UC1, CV_32SC1, CV_8UC3, Vec3b};
+use opencv::core::{Point, Scalar, Vector, no_array, min_max_loc, CV_8UC1, CV_32SC1, CV_8UC3, Vec3b};
 use opencv::imgcodecs::{imread, imwrite, IMREAD_COLOR};
-use opencv::imgproc::cvt_color;
 use opencv::prelude::*;
 use std::time::Instant;
 
 fn main() {
-    let mut image = imread("medium.jpg", IMREAD_COLOR).unwrap();
+    let image = imread("medium.jpg", IMREAD_COLOR).unwrap();
 
     let threshold = 2000f32; // TODO: Revisit after distance normalization TODOs are addressed
     let mut segmenter = Segmentation::new(EuclideanRGB::default(), MagicThreshold::new(threshold));
@@ -37,10 +36,10 @@ fn main() {
     let mut max = 0f64;
     let mut min_loc = Point::default();
     let mut max_loc = Point::default();
-    min_max_loc(&labels, &mut min, &mut max, &mut min_loc, &mut max_loc, &no_array().unwrap());
+    min_max_loc(&labels, &mut min, &mut max, &mut min_loc, &mut max_loc, &no_array().unwrap()).unwrap();
 
     let mut labels_out = Mat::default().unwrap();
-    labels.convert_to(&mut labels_out, CV_8UC1, 255f64/max, 0f64);
+    labels.convert_to(&mut labels_out, CV_8UC1, 255f64/max, 0f64).unwrap();
     imwrite("labels.jpg", &labels_out, &Vector::default()).unwrap();
 
     let contours = draw_contours(&image, &labels).unwrap();
